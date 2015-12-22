248612a7357fbc36b47d74316d826b0cd06dee29
YDSS <y.dsheng10@gmail.com>
2015-12-22 21:43:00 +0800

    change enter file for ali cloud

---
+++ main.js
@@ -0,0 +1,16 @@
+var app = require('./app');
+var http = require('http');
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+// 阿里云不允许自己绑定端口，listen传空值就可以
+// server.listen();
+server.listen(3000);
