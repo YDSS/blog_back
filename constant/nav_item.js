924af4b8178ce993b489187d2d4bfa809112cc5e
YDSS <y.dsheng10@gmail.com>
2016-02-13 23:09:59 +0800

    nav authorize

--- constant/nav_item.js
+++ constant/nav_item.js
@@ -1,3 +1,5 @@
+import {cloneDeep} from 'lodash/Lang'; 
+
 const NAV_TYPE = {
     // 指向内部router的链接, 如/edit
     INNER: 0,
@@ -52,29 +54,22 @@ const navItems = [
     }
 ];
 
-const authRestrictedList = ['/edit'];
+const restrictedList = ['/edit', '/diary/upload'];
     
 /**
  * 获取导航栏列表，非ydss不显示操作相关的item，
  * 如edit
  *
- * @param {boolean} auth 是否为作者（ydss）
+ * @param {boolean} auth 是否有权限(cookie中有auth=ydss)
  * @return {Array} 导航栏列表
  */
 export default function getNavItems(auth) {
     let ret = [];
+    // 深拷贝，每次请求都会重新计算
+    let copyItems = cloneDeep(navItems);
  
     if (!auth) {
-        ret = navItems.filter(item => {
-            let flag = true;
-            authRestrictedList.map(e => {
-                if (e === item.url) {
-                    flag = false;
-                }
-            });
-
-            return flag;
-        });
+        copyItems.map(item => iterateNavItems(item, ret));
     }
     else {
         ret = navItems;
@@ -83,3 +78,42 @@ export default function getNavItems(auth) {
     return ret;
 }
 
+/**
+ * 遍历导航栏列表，去掉受限制的item
+ *
+ * @param {Object} node 列表中的节点，只有叶子节点和非叶子节点之分，
+ *  type为NAV_TYPE.NO_LEAF则表示有子节点,
+ *  最多只有2层
+ */
+function iterateNavItems(node, result) {
+    // node是否在受限列表中
+    if (restrictedList.every(item => item !== node.url)) {
+        // 如果node有子节点，则对子节点进行过滤
+        if (node.type === NAV_TYPE.NO_LEAF) {
+            node = filterChildren(node.children, node);
+        }
+
+        node && result.push(node);
+    }
+}
+
+/**
+ * 过滤子节点中受限制的item
+ *
+ * @param {Array} children 子节点集合
+ * @param {Object} parent 父节点
+ * @return {Object} 过滤子节点之后的父节点
+ */
+function filterChildren(children, parent) {
+    let newChildren = children.filter(child => {
+        return restrictedList.every(item => item !== child.url);
+    });
+
+    // 若过滤之后已没有子节点，则把该子节点的父节点去掉
+    if (!newChildren.length) {
+        return null;
+    }
+
+    parent.children = newChildren;
+    return parent;
+}
