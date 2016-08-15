5c1cc90d4b0a1eff5da77e50ddeb60a2e8647cc2
YDSS <y.dsheng10@gmail.com>
2016-08-15 20:56:01 +0800

    migrate post(article) ajax api to koa

--- register.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file 注册babel register，运行时编译es5,
- *  真实app入口为main.js
- */
-require('babel-core/register');
-// ace的node版本太低，没有原生Promise，用es6-promise作为polyfill
-require('es6-promise').polyfill();
-
-require('./main');
